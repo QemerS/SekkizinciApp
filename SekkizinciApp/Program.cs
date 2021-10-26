7a9ec6a2506140e555a05ae58da0be39599de369
QemerS <tu201806169@code.edu.az>
2021-10-26 05:43:13 +0400

    Add project files.

---
+++ SekkizinciApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SekkizinciApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Write a C# program that takes (with Console.ReadLine() method) a number
+            and width also a number, as input and then displays a triangle of that width,using that number.
+             */
+
+            Console.WriteLine("Enter a number"); int number =  Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("Enter the desired width"); int width = Convert.ToInt32( Console.ReadLine());
+
+            while (width >= 0)
+            {
+                Console.WriteLine();
+                for (int a = width; a > 0; a--)
+                {
+                    Console.Write((Convert.ToString(number)));
+                }
+                width--;
+            }
+        }
+    }
+}
